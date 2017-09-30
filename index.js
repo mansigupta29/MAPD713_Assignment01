7731dfbe1a6e8c385ca8c0157ccd51b24c716306
Mansi Gupta <mansigupta29>
2017-09-30 10:43:12 +0530

    updated version

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var SERVER_NAME = 'stationaryStore-api'
+var SERVER_NAME = 'students-api'
 var PORT = 8000;
 var HOST = '127.0.0.1';
 
 
 var restify = require('restify')
-  , stationaryStore = require('save')('stationaryStore')
+  , studentsSave = require('save')('students')
   , server = restify.createServer({ name: SERVER_NAME})
 
   server.listen(PORT, HOST, function () {
@@ -12,3 +12,83 @@ var restify = require('restify')
 })
 
 server .use(restify.fullResponse()).use(restify.bodyParser())
+
+
+server.get('/students', function (req, res, next) {
+    studentsSave.find({}, function (error, students) {
+      res.send(students)
+    })
+  })
+
+  server.get('/students/:id', function (req, res, next) {
+    studentsSave.findOne({ _id: req.params.id }, function (error, student) {
+        if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+    
+        if (student) {
+          res.send(student)
+        } else {
+          res.send("Student Not Found!!")
+        }
+      })
+    })
+
+    server.post('students', function(req, res, next){
+
+      if (req.params.name === undefined) {
+        return next(new restify.InvalidArgumentError('Name Required!!'))
+      }
+      if (req.params.course === undefined ) {
+        return next(new restify.InvalidArgumentError('Course Required!!'))
+      }
+
+      var newStudent = {
+        name: req.params.name, 
+        course: req.params.course
+      }
+
+      studentsSave.create( newStudent, function (error, student) {
+        
+            if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+        
+            res.send(201, student)
+          })
+    })
+
+    server.put('/students/:id', function (req, res, next) {
+      
+        if (req.params.name === undefined ) {
+          return next(new restify.InvalidArgumentError('Name Required!!'))
+        }
+        if (req.params.course === undefined ) {
+          return next(new restify.InvalidArgumentError('Course Required!!'))
+        }
+        
+        var newStudent = {
+          _id: req.params.id,
+          name: req.params.name, 
+          course: req.params.course
+        }
+
+        studentsSave.update(newStudent, function (error, student) {
+          
+              // If there are any errors, pass them to next in the correct format
+              if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+          
+              // Send a 200 OK response
+              res.send(200)
+            })
+          })
+          
+
+          server.del('/students/:id', function (req, res, next) {
+            
+              studentsSave.delete(req.params.id, function (error, student) {
+            
+                // If there are any errors, pass them to next in the correct format
+                if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+            
+                // Send a 200 OK response
+                res.send()
+              })
+            })
+            
